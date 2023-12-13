3b9b3b7ff056491a7a5641ec1cbd03b30c104df5
Michał Paduch (INI) <INI@kmd.dk>
2023-12-13 08:43:12 +0100

    Rewrite to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { InputMaskModule } from 'primeng/inputmask';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    InputMaskModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
