e686012baa940485a770758fd5d30ad076dad7ab
Michał Paduch (INI) <INI@kmd.dk>
2023-12-13 10:40:45 +0100

    Add hiding issue

--- src/setup.jest.ts
+++ src/setup.jest.ts
@@ -1,2 +1,13 @@
 // setupJest.ts
 import 'jest-preset-angular/setup-jest';
+
+/*
+Uncomment this to hide error
+const _ = require('lodash')
+const originalConsoleError = console.error
+console.error = function(msg) {
+  if(_.startsWith(msg, '[vuex] unknown')) return
+  if(_.startsWith(msg, 'Error: Could not parse CSS stylesheet')) return
+  originalConsoleError(msg)
+}
+*/
